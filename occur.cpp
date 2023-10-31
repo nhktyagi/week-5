eaf61f7a69186eadc3d9a9cdbb0d7edcbd53b27e
nhktyagi <84965999+nhktyagi@users.noreply.github.com>
2023-10-31 16:57:07 +0530

    Add files via upload

---
+++ occur.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+	int n;
+	printf("Enter the number of elements in the array");
+	scanf("%d",&n);
+	int arr[n];
+	printf("Enter the elements");
+	for (int i=0;i<n;i++)
+	{
+		scanf("%d",&arr[i]);
+		
+	}
+
